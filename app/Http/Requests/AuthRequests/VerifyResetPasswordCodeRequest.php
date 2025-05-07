42f834ed4faf9494c08b8414bce8d38559118817
RedouaneKhlifahLf <redouane.khlifah@araba.shop>
2025-05-07 12:13:11 +0100

    Remove unnecessary email existence check from verification rules

--- app/Http/Requests/AuthRequests/VerifyResetPasswordCodeRequest.php
+++ app/Http/Requests/AuthRequests/VerifyResetPasswordCodeRequest.php
@@ -22,7 +22,7 @@ class VerifyResetPasswordCodeRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => 'required|email|exists:users,email',
+            'email' => 'required|email',
             'verification_code' => 'required|digits:4',
         ];
     }
